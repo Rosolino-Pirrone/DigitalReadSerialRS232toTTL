df7621bc5a810fdcd5b9f9fcb93633bde81b89bc
Rosolino Pirrone <rosolinopirrone@libero.it>
2022-05-06 17:14:08 +0200

    Update DigitalReadSerialRS232toTTL.ino

--- DigitalReadSerialRS232toTTL.ino
+++ DigitalReadSerialRS232toTTL.ino
@@ -1,5 +1,4 @@
-#include <SoftwareSerial.h>
-SoftwareSerial ss (11, 12);
+
 /*
   DigitalReadSerial
 
@@ -19,8 +18,8 @@ SoftwareSerial ss (11, 12);
 
 #define bit9600Delay 100  
 #define halfBit9600Delay 50
-#define bit4800Delay 191
-#define halfBit4800Delay 95
+#define bit4800Delay 192
+#define halfBit4800Delay 96
 
 
 byte SWval;
@@ -42,50 +41,10 @@ void setup() {
   pinMode(outputTtl, OUTPUT);
   pinMode(inputTtl, INPUT);
   pinMode(outputRs232_3v, OUTPUT);
-  ss.begin(9600);
-
   
 }
 
-void SWprint(int data)
-{
-  byte mask;
-  //startbit
-  digitalWrite(outputRs232_3v,HIGH);
-  delayMicroseconds(bit4800Delay / 2);
-  for (mask = 0x01; mask>0; mask <<= 1) {
-    if (data & mask){ // choose bit
-     digitalWrite(outputRs232_3v,LOW); // send 1
-    }
-    else{
-     digitalWrite(outputRs232_3v,HIGH); // send 0
-    }
-    delayMicroseconds(bit4800Delay / 2);
-  }
-  //stop bit
-  digitalWrite(outputRs232_3v, LOW);
-  delayMicroseconds(bit4800Delay / 2);
-}
 
-int SWread()
-{
-  byte val = 0;
-  while (!digitalRead(inputRs232_3v));
-  //wait for start bit
-  if (digitalRead(inputRs232_3v)) {
-    delayMicroseconds(halfBit4800Delay / 2);
-    for (int offset = 0; offset < 8; offset++) {
-     delayMicroseconds(bit4800Delay / 2);
-     val |= !digitalRead(inputRs232_3v) << offset;
-    }
-    //wait for stop bit + extra
-    delayMicroseconds(bit4800Delay / 2);
-    delayMicroseconds(bit4800Delay / 2);
-    //delayMicroseconds(bit4800Delay);
-    //delayMicroseconds(bit4800Delay);
-    return val;
-  }
-}
 
 // the loop routine runs over and over again forever:
 void loop() {
